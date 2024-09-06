27e044a6c5356bab26484e52c8f10c1efad9ca42
cuonghx <cuonghx@gmail.com>
2024-09-06 09:48:47 +0700

    Add payload

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,28 @@
 import { Controller, Get, Post, Render } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Bot } from 'grammy';
 
 @Controller()
 export class AppController {
+  private readonly bot: Bot;
+  constructor(private readonly configService: ConfigService) {
+    this.bot = new Bot(this.configService.get('TELEGRAM_BOT_TOKEN'));
+  }
+
   @Get()
   @Render('index')
-  root() {}
+  async root() {
+    const invoiceLink = await this.bot.api.createInvoiceLink(
+      'Test Product',
+      'Test Description',
+      'Test Payload',
+      '',
+      'XTR',
+      [{ label: 'stars', amount: 1 }],
+    );
+    return { invoiceLink };
+  }
 
   @Post('api/purchase')
-  purchase() {
-    return { message: 'API call successful!' };
-  }
+  purchase() {}
 }
